c2c048706010e3734057254f7b82b098d87178c3
sebasvv12 <svallejov@correo.iue.edu.co>
2021-05-26 21:28:34 -0500

    Updated comments

---
+++ cod.js
@@ -0,0 +1,7 @@
+function changeColor(newColor, identificador)
+{
+    var elem=document.getElementById(identificador);
+    console.log(newColor);
+    elem.style.background = newColor;
+}
+
--- jquery.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(() => {
-    $('#datos').hover(function(){
-        $(this).css('background-color','black');
-    });
-});
- 
